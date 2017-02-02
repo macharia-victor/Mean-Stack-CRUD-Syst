6e0d576f357363d04d27c06d061e99690e41a769
macharia-victor <machvik1@gmail.com>
2017-02-02 03:25:54 +0300

    mean files

---
+++ public/controllers/controller.js
@@ -0,0 +1,25 @@
+function AppCtrl($scope, $http){
+	console.log("Hello, this is the controller");
+
+var refresh = function(){
+	$http.get('/contactlist').success(function(response){
+		console.log("I got the data I requested.")
+		$scope.contactlist = response;
+		$scope.contact = "";
+
+});
+};
+
+refresh();
+
+$scope.addContact = function(){
+	console.log($scope.contact);
+	$http.post('/contactlist', $scope.contact).success(function(response){
+		console.log(response);
+		refresh();
+	});
+		
+	
+};
+	
+}
